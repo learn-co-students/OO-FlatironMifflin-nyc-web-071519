dbff51b32a25e983a8c75e2c9eeefa5292fc1975
Tyler Knapp <tgknapp11@gmail.com>
2019-07-18 13:52:06 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,38 @@
 class Employee
 
+    attr_accessor :department, :salary
+    attr_reader :name 
+
+    @@all = []
+    # @@tax_bracket = []
+
+    def initialize(attr)
+        @name = attr[:name]
+        @department = attr[:department]
+        @salary = attr[:salary]
+        @@all << self
+    end
+
+    def paid_over
+
+    end
+
+    def find_by_department
+        
+    end
+
+    def tax_bracket
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def tax_bracket
+
+    end
+
 end
+
+# Employee.new({name:, department:, salary:})
